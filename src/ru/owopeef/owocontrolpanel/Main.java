a021f1d014dd0374f87604c8531cb5982832a714
Anatoliy Morozan <77969185+owoPeef@users.noreply.github.com>
2021-08-08 17:51:35 +0300

    Added control panel menu and command

--- src/ru/owopeef/owocontrolpanel/Main.java
+++ src/ru/owopeef/owocontrolpanel/Main.java
@@ -1,12 +1,13 @@
 package ru.owopeef.owocontrolpanel;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.owopeef.owocontrolpanel.commands.Commands;
 
 public class Main extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
-        
+        getCommand("control_panel").setExecutor(new Commands());
     }
 }
