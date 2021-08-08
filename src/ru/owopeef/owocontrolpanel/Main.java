4369cecc631b483b74c0aa48fdc145345e7f1672
Anatoliy Morozan <77969185+owoPeef@users.noreply.github.com>
2021-08-08 15:36:29 +0300

    New project

---
+++ src/ru/owopeef/owocontrolpanel/Main.java
@@ -0,0 +1,12 @@
+package ru.owopeef.owocontrolpanel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+}
